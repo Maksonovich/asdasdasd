9118b97d56ae4fdd3177672e4db3d02c6d6855cc
Aidar satubaldiev <aydatsatubaldiev@gmail.com>
2023-02-22 01:32:26 +0600

    done

--- src/pages/Decoration/decoration.jsx
+++ src/pages/Decoration/decoration.jsx
@@ -34,10 +34,10 @@ const Decoration = () => {
   const [active, setActive] = useState(false);
 
   function eeeee() {
-      setActive(active === false)
+    setActive(active === false);
   }
   function bbbbb(e) {
-      setActive(active = true)
+    setActive((active = true));
   }
 
   return (
@@ -82,144 +82,161 @@ const Decoration = () => {
         {task.length !== 0 && (
           <div className="Making__blocks">
             <div className="Making__blocks-left">
-              <div className="Making__blocks-left_title">
-                ВЫБЕРИТЕ СПОСОБ ДОСТАВКИ
-              </div>
-              <div className="Making__blocks-left_check">
-                <div className="Making__blocks-left_check-form">
-                  <label>
-                    <input type="radio" name="deliver" onChange={eeeee}/>
-                    <b></b>
-                    <span>Шоурум</span>
-                  </label>
-                  <label>
-                    <input type="radio" name="deliver"  onChange={eeeee}/>
-                    <b></b>
-                    <span>Курьером по Москве</span>
-                  </label>
-                  <label>
-                    <input type="radio" name="deliver" onChange={bbbbb}/>
-                    <b></b>
-                    <span>Курьером по России</span>
-                  </label>
-                  <label>
-                    <input type="radio" name="deliver" onChange={bbbbb}/>
-                    <b></b>
-                    <span>Курьером за границу</span>
-                  </label>
+              <form action="/djkama">
+                <div className="Making__blocks-left_title">
+                  ВЫБЕРИТЕ СПОСОБ ДОСТАВКИ
                 </div>
-              </div>
-              {active && (
-                <div className="Making__blocks-left_select">
-                  <div className="Making__blocks-left_select-inner">
-                    <div className="Making__blocks-left_select-top">
-                      <input
-                        placeholder="Выберите дату"
-                        type="text"
-                        onClick={() => SetArrow(!arrow)}
-                      />
+                <div className="Making__blocks-left_check">
+                  <div className="Making__blocks-left_check-form">
+                    <label>
+                      <input type="radio" name="deliver" onChange={eeeee} />
+                      <b></b>
+                      <span>Шоурум</span>
+                    </label>
+                    <label>
+                      <input type="radio" name="deliver" onChange={eeeee} />
+                      <b></b>
+                      <span>Курьером по Москве</span>
+                    </label>
+                    <label>
+                      <input type="radio" name="deliver" onChange={bbbbb} />
+                      <b></b>
+                      <span>Курьером по России</span>
+                    </label>
+                    <label>
+                      <input type="radio" name="deliver" onChange={bbbbb} />
+                      <b></b>
+                      <span>Курьером за границу</span>
+                    </label>
+                  </div>
+                </div>
+                {active && (
+                  <div className="Making__blocks-left_select">
+                    <div className="Making__blocks-left_select-inner">
+                      <div className="Making__blocks-left_select-top">
+                        <input
+                          placeholder="Выберите дату"
+                          type="text"
+                          onClick={() => SetArrow(!arrow)}
+                        />
+                        <div
+                          style={{ rotate: `${arrow ? "180deg" : "0deg"}` }}
+                        ></div>
+                      </div>
                       <div
-                        style={{ rotate: `${arrow ? "180deg" : "0deg"}` }}
-                      ></div>
+                        className={`Making__blocks-left_select-bottom ${
+                          arrow ? "actives" : ""
+                        }`}
+                      >
+                        <div className="Making__blocks-left_select-bottom_item">
+                          <input type="hidden" />
+                          Сегодня, 29 февраля
+                        </div>
+                        <div className="Making__blocks-left_select-bottom_item">
+                          <input type="hidden" />
+                          Завтра, 30 февраля
+                        </div>
+                        <div className="Making__blocks-left_select-bottom_item">
+                          <input type="hidden" />
+                          среда, 15 февраля
+                        </div>
+                      </div>
                     </div>
-                    <div
-                      className={`Making__blocks-left_select-bottom ${
-                        arrow ? "actives" : ""
-                      }`}
-                    >
-                      <div className="Making__blocks-left_select-bottom_item">
-                        <input type="hidden" />
-                        Сегодня, 29 февраля
+                    <div className="Making__blocks-left_select-inner">
+                      <div className="Making__blocks-left_select-top">
+                        <input type="text" onClick={() => SetList(!list)} />
+                        <div
+                          style={{ rotate: `${list ? "180deg" : "0deg"}` }}
+                        ></div>
                       </div>
-                      <div className="Making__blocks-left_select-bottom_item">
-                        <input type="hidden" />
-                        Завтра, 30 февраля
-                      </div>
-                      <div className="Making__blocks-left_select-bottom_item">
-                        <input type="hidden" />
-                        среда, 15 февраля
+                      <div
+                        className={`Making__blocks-left_select-bottom ${
+                          list ? "actives" : ""
+                        }`}
+                      >
+                        <div className="Making__blocks-left_select-bottom_item">
+                          <input type="hidden" />С 12 до 15 часов
+                        </div>
+                        <div className="Making__blocks-left_select-bottom_item">
+                          <input type="hidden" />С 16 до 19 часов
+                        </div>
+                        <div className="Making__blocks-left_select-bottom_item">
+                          <input type="hidden" />С 7 до 16 часов
+                        </div>
                       </div>
                     </div>
                   </div>
-                  <div className="Making__blocks-left_select-inner">
-                    <div className="Making__blocks-left_select-top">
-                      <input type="text" onClick={() => SetList(!list)} />
-                      <div
-                        style={{ rotate: `${list ? "180deg" : "0deg"}` }}
-                      ></div>
-                    </div>
-                    <div
-                      className={`Making__blocks-left_select-bottom ${
-                        list ? "actives" : ""
-                      }`}
-                    >
-                      <div className="Making__blocks-left_select-bottom_item">
-                        <input type="hidden" />С 12 до 15 часов
-                      </div>
-                      <div className="Making__blocks-left_select-bottom_item">
-                        <input type="hidden" />С 16 до 19 часов
-                      </div>
-                      <div className="Making__blocks-left_select-bottom_item">
-                        <input type="hidden" />С 7 до 16 часов
-                      </div>
-                    </div>
+                )}
+                <div className="Making__blocks-left_inputs">
+                  <div className="Making__blocks-left_inputs-title">
+                    Основная информация
+                  </div>
+                  <div className="Making__blocks-left_inputs-item">
+                    <input type="text" placeholder="Введите фио"  required/>
+                  </div>
+                  <div className="Making__blocks-left_inputs-item">
+                    <input type="number" placeholder="Введите телефон" required/>
+                  </div>
+                  <div className="Making__blocks-left_inputs-item">
+                    <input type="email" placeholder="Введите e-mail" required/>
+                  </div>
+                  <div className="Making__blocks-left_inputs-textarea">
+                    <textarea
+                      name=""
+                      id=""
+                      cols="30"
+                      rows="10"
+                      placeholder="Добавить коментарии"
+                    ></textarea>
                   </div>
                 </div>
-              )}
-              <div className="Making__blocks-left_inputs">
-                <div className="Making__blocks-left_inputs-title">Основная информация</div>
-                <div className="Making__blocks-left_inputs-item">
-                  <input type="text" placeholder="Введите фио"/>
+                <div className="Making__blocks-left_bold">
+                  Выберите способ оплаты
                 </div>
-                <div className="Making__blocks-left_inputs-item">
-                  <input type="number" placeholder="Введите телефон"/>
+                <div className="Making__blocks-left_radios">
+                  <label className="Making__blocks-left_radios-item">
+                    <input type="radio" name="radiosa" />
+                    <b></b>
+                    <span>Оплата картой</span>
+                  </label>
+                  <label className="Making__blocks-left_radios-item">
+                    <input type="radio" name="radiosa" />
+                    <b></b>
+                    <span>Оплата при получении</span>
+                  </label>
+                  <label className="Making__blocks-left_radios-item">
+                    <input type="radio" name="radiosa" />
+                    <b></b>
+                    <span>Купить в рассрочку</span>
+                  </label>
                 </div>
-                <div className="Making__blocks-left_inputs-item">
-                  <input type="email" placeholder="Введите e-mail"/>
+                <div className="Making__blocks-left_custom">
+                  <div className="Making__blocks-left_custom-price">
+                    <div>Предварительная стоимость:</div>
+                    <div>{totalPrice - 1500} ₽</div>
+                  </div>
+                  <div className="Making__blocks-left_custom-price">
+                    <div>Курьером за границу:</div>
+                    <div>{totalPrice / 5} ₽</div>
+                  </div>
+                  <div className="Making__blocks-left_custom-price">
+                    <div>Итого:</div>
+                    <div>{totalPrice} ₽</div>
+                  </div>
                 </div>
-                <div className="Making__blocks-left_inputs-textarea">
-                  <textarea name="" id="" cols="30" rows="10" placeholder="Добавить коментарии"></textarea>
-                </div>
-              </div>
-              <div className="Making__blocks-left_bold">Выберите способ оплаты</div>
-              <div className="Making__blocks-left_radios">
-                <label className="Making__blocks-left_radios-item">
-                  <input type="radio" name="radiosa" />
+                <label className="Making__blocks-left_label">
+                  <input type="checkbox" required/>
                   <b></b>
-                  <span>Оплата картой</span>
+                  <span>
+                    Я принимаю <a>договор публичной оферты</a>
+                  </span>
                 </label>
-                <label className="Making__blocks-left_radios-item">
-                  <input type="radio" name="radiosa" />
-                  <b></b>
-                  <span>Оплата при получении</span>
-                </label>
-                <label className="Making__blocks-left_radios-item">
-                  <input type="radio" name="radiosa" />
-                  <b></b>
-                  <span>Купить в рассрочку</span>
-                </label>
-              </div>
-              <div className="Making__blocks-left_custom">
-                <div className="Making__blocks-left_custom-price">
-                  <div>Предварительная стоимость:</div>
-                  <div>{totalPrice - 1500} ₽</div>
-                </div>
-                <div className="Making__blocks-left_custom-price">
-                  <div>Курьером за границу:</div>
-                  <div>{totalPrice / 5} ₽</div>
-                </div>
-                <div className="Making__blocks-left_custom-price">
-                  <div>Итого:</div>
-                  <div>{totalPrice} ₽</div>
-                </div>
-              </div>
-              <label className="Making__blocks-left_label">
-                <input type="checkbox" />
-                <b></b>
-                <span>Я принимаю <a>договор публичной оферты</a></span>
-              </label>
-              <button className="Making__blocks-left_btn" type="submit">Оформить заказ</button>
+                <button className="Making__blocks-left_btn" type="submit">
+                  Оформить заказ
+                </button>
+              </form>
             </div>
+
             <div className="Making__blocks-right">
               <img
                 src="https://eleanboutique.ru/image/catalog/demo/theme/cat/mob/oimg/delivery-1.jpg"
