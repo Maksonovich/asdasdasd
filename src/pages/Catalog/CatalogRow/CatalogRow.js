4feb9de4419cce320344af6d35a78569a4f965e5
Aidar satubaldiev <aydatsatubaldiev@gmail.com>
2023-02-22 17:33:04 +0600

    some change

--- src/pages/Catalog/CatalogRow/CatalogRow.js
+++ src/pages/Catalog/CatalogRow/CatalogRow.js
@@ -1,36 +1,54 @@
-import React, { useState } from "react";
-import { useDispatch } from "react-redux";
-import { Link } from "react-router-dom";
-import { MakeAdd, setFavorites } from "../../../redux/reducers/dataCrad";
+  import React, { useState } from "react";
+  import { useDispatch } from "react-redux";
+  import { Link } from "react-router-dom";
+  import { MakeAdd, setFavorites } from "../../../redux/reducers/dataCrad";
 
-const CatalogRow = ({ products }) => {
-  const dispatch = useDispatch();
-  const [ selectedIndex, setSelectedIndex ] = useState('')
-  return (
-    <div className="catalog_row">
-      {products.data.map((item, active) => (
-        <div className="catalog__card" key={item.id}>
-          <img className="catalog__card-img" src={item.img} alt={item.title} />
-          <img
-            className="catalog__card-image"
-            onClick={() => dispatch(setFavorites("favorite", item)) || setSelectedIndex(active)}
-            src={item.image}
-            alt={item.image}
-            style={{ border: active === selectedIndex ? '2px solid #00adb5' : 'none'}}
-          />
-          <Link
-          to="/MakeAnOrder"
-            className="catalog__card-title"
-            onClick={() => dispatch(MakeAdd("makeOrder", item))}
-          >
-            {item.title}
-          </Link>
-          <p className="catalog__card-price">{item.price} ₽</p>
-          <p className="catalog__card-text">{item.text}</p>
-        </div>
-      ))}
-    </div>
-  );
-};
+  const CatalogRow = ({ products }) => {
+    const dispatch = useDispatch();
+    const [activeIndex, setActiveIndex] = useState("");
+    
+    const handleFavoriteClick = (item) => {
+      dispatch(setFavorites("favorite", item));
+      setActiveIndex(item.id);
+    };
+    
+    return (
+      <div className="catalog_row">
+        {products.data.map((item) => (
+          <div className="catalog__card" key={item.id}>
+            <img className="catalog__card-img" src={item.img} alt={item.title} />
+            <svg
+              className="catalog__card-image"
+              onClick={() => handleFavoriteClick(item)}
+              src={item.image}
+              alt={item.image}
+              style={{
+                fill: item.id === activeIndex ? "#000" : "none",
+              }}
+              width="23"
+              height="20"
+              viewBox="0 0 23 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M11.5703 3.70142C10.9351 3.10858 8.50169 0.978395 6.34687 1.00017C4.13844 1.02277 0.729548 2.52245 1.01707 6.9662C1.2713 10.8916 11.4013 18.9636 11.4013 18.9636C11.4647 19.0121 11.5305 19.0121 11.5987 18.9636C11.5987 18.9636 21.7287 10.8908 21.9829 6.9662C22.2705 2.52245 18.8616 1.02277 16.6523 1.00017C14.1499 0.975046 11.2714 3.85214 11.2714 3.85214L9.31716 5.91953"
+                stroke="#222222"
+                stroke-miterlimit="3.8637"
+              />
+            </svg>
+            <Link
+              to="/MakeAnOrder"
+              className="catalog__card-title"
+              onClick={() => dispatch(MakeAdd("makeOrder", item))}
+            >
+              {item.title}
+            </Link>
+            <p className="catalog__card-price">{item.price} ₽</p>
+            <p className="catalog__card-text">{item.text}</p>
+          </div>
+        ))}
+      </div>
+    );
+  };
 
-export default CatalogRow;
+  export default CatalogRow;
--- src/pages/Home/Otziv/Otziv.jsx
+++ src/pages/Home/Otziv/Otziv.jsx
@@ -73,76 +73,74 @@ const Otziv = () => {
                         </div>
                     </div>
                 </div>
-            </div>
-            {
+                {
                 open && 
-            <div className="Otziv_inner-dop">
-                <div className="Otziv_inner_l">
-                    <div className="Otziv_inner_l_content">
-                        <img className='Otziv_inner_l_icon' src={iconcom} alt="" />
-                        <div className="Otziv_inner_l_coment">
-                            <div className="Otziv_inner_l_coment_p">
-                                 <p >Ирина <span> г. Москва</span> </p>
-                                <p>26 Марта 2017</p>
-                            </div>
-                            <div className="Otziv_inner_l_coment_text">
-                                <p>Да, доставка заграницу осуществляется курьерской службой до 
-                                    двери. Доставка заграницу оплачивается при оформлении 
-                                    заказа, фиксированная стоимость 1 200₽.
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="Otziv_inner_l_content">
-                        <img className='Otziv_inner_l_icon' src={iconcom} alt="" />
-                        <div className="Otziv_inner_l_coment">
-                            <div className="Otziv_inner_l_coment_p">
-                                 <p >Ирина <span> г. Москва</span> </p>
-                                <p>26 Марта 2017</p>
-                            </div>
-                            <div className="Otziv_inner_l_coment_text">
-                                <p>Да, доставка заграницу осуществляется курьерской службой до 
-                                    двери. Доставка заграницу оплачивается при оформлении 
-                                    заказа, фиксированная стоимость 1 200₽.
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="Otziv_inner_l">
-                    <div className="Otziv_inner_l_content">
-                        <img className='Otziv_inner_l_icon' src={iconcom} alt="" />
-                        <div className="Otziv_inner_l_coment">
-                            <div className="Otziv_inner_l_coment_p">
-                                <p >Ирина <span> г. Москва</span> </p>
-                                <p>26 Марта 2017</p>
-                            </div>
-                            <div className="Otziv_inner_l_coment_text">
-                                <p>Да, доставка заграницу осуществляется курьерской службой до 
-                                    двери. Доставка заграницу оплачивается при оформлении 
-                                    заказа, фиксированная стоимость 1 200₽.
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="Otziv_inner_l_content">
-                        <img className='Otziv_inner_l_icon' src={iconcom} alt="" />
-                        <div className="Otziv_inner_l_coment">
-                            <div className="Otziv_inner_l_coment_p">
-                                <p >Ирина <span> г. Москва</span> </p>
-                                <p>26 Марта 2017</p>
-                            </div>
-                            <div className="Otziv_inner_l_coment_text">
-                                <p>Да, доставка заграницу осуществляется курьерской службой до 
-                                    двери. Доставка заграницу оплачивается при оформлении 
-                                    заказа, фиксированная стоимость 1 200₽.
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+                <><div className="Otziv_inner_l">
+                          <div className="Otziv_inner_l_content">
+                              <img className='Otziv_inner_l_icon' src={iconcom} alt="" />
+                              <div className="Otziv_inner_l_coment">
+                                  <div className="Otziv_inner_l_coment_p">
+                                      <p>Ирина <span> г. Москва</span> </p>
+                                      <p>26 Марта 2017</p>
+                                  </div>
+                                  <div className="Otziv_inner_l_coment_text">
+                                      <p>Да, доставка заграницу осуществляется курьерской службой до
+                                          двери. Доставка заграницу оплачивается при оформлении
+                                          заказа, фиксированная стоимость 1 200₽.
+                                      </p>
+                                  </div>
+                              </div>
+                          </div>
+                          <div className="Otziv_inner_l_content">
+                              <img className='Otziv_inner_l_icon' src={iconcom} alt="" />
+                              <div className="Otziv_inner_l_coment">
+                                  <div className="Otziv_inner_l_coment_p">
+                                      <p>Ирина <span> г. Москва</span> </p>
+                                      <p>26 Марта 2017</p>
+                                  </div>
+                                  <div className="Otziv_inner_l_coment_text">
+                                      <p>Да, доставка заграницу осуществляется курьерской службой до
+                                          двери. Доставка заграницу оплачивается при оформлении
+                                          заказа, фиксированная стоимость 1 200₽.
+                                      </p>
+                                  </div>
+                              </div>
+                          </div>
+                      </div><div className="Otziv_inner_l">
+                              <div className="Otziv_inner_l_content">
+                                  <img className='Otziv_inner_l_icon' src={iconcom} alt="" />
+                                  <div className="Otziv_inner_l_coment">
+                                      <div className="Otziv_inner_l_coment_p">
+                                          <p>Ирина <span> г. Москва</span> </p>
+                                          <p>26 Марта 2017</p>
+                                      </div>
+                                      <div className="Otziv_inner_l_coment_text">
+                                          <p>Да, доставка заграницу осуществляется курьерской службой до
+                                              двери. Доставка заграницу оплачивается при оформлении
+                                              заказа, фиксированная стоимость 1 200₽.
+                                          </p>
+                                      </div>
+                                  </div>
+                              </div>
+                              <div className="Otziv_inner_l_content">
+                                  <img className='Otziv_inner_l_icon' src={iconcom} alt="" />
+                                  <div className="Otziv_inner_l_coment">
+                                      <div className="Otziv_inner_l_coment_p">
+                                          <p>Ирина <span> г. Москва</span> </p>
+                                          <p>26 Марта 2017</p>
+                                      </div>
+                                      <div className="Otziv_inner_l_coment_text">
+                                          <p>Да, доставка заграницу осуществляется курьерской службой до
+                                              двери. Доставка заграницу оплачивается при оформлении
+                                              заказа, фиксированная стоимость 1 200₽.
+                                          </p>
+                                      </div>
+                                  </div>
+                              </div>
+                          </div></>
             }
+            </div>
+            
             <div className="Otziv_button" onClick={() => isOpen(!open)}>{`${open ? 'ЗАКРЫТЬ' : 'СМОТРЕТЬ ВСЁ'}`}</div>
         </div> 
     </section>
