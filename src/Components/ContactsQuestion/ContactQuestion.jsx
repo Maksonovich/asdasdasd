ad00acaf4d67811cb6a29511f8a6b10ab0dc72fd
Aidar satubaldiev <aydatsatubaldiev@gmail.com>
2023-02-28 00:55:09 +0600

    s

--- src/Components/ContactsQuestion/ContactQuestion.jsx
+++ src/Components/ContactsQuestion/ContactQuestion.jsx
@@ -1,105 +1,110 @@
-import React, { useState } from 'react'
-import { TbHanger } from 'react-icons/tb'
+import React, { useState } from "react";
+import { TbHanger } from "react-icons/tb";
 
 const ContactQuestion = () => {
+  const iconStyle = {
+    width: "300px",
+    height: "60px",
+    padding: "20px",
+  };
 
-    const iconStyle = {
-        fill: "#C0A78F",
-        fontSize: "30px"
-    }
+  const inputStyle = {
+    fill: "#C0A78F",
+    fontSize: "30px",
+  };
 
-    const inputStyle = {
-        width: "270px",
-        height: "60px",
-        padding: "30px",
-    }
+  const [name, setName] = useState("");
+  const [data, setData] = useState("");
+  const [mobileNumber, setMobileNumber] = useState("");
+  const [message, setMessage] = useState("");
 
-    const [name, setName] = useState("")
-    const [data, setData] = useState("");
-    const [mobileNumber, setMobileNumber] = useState("");
-    const [message, setMessage] = useState("");
+  let url = "http://localhost:3002/showRoom";
 
-    let url = 'http://localhost:3002/showRoom'
+  let handleSubmit = async (e) => {
+    fetch(url, {
+      method: "POST",
+      body: JSON.stringify({
+        name: e.target[0].value,
+        mobileNumber: e.target[1].value,
+        data: e.target[2].value,
+        message: e.target[3].value,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  };
 
-    let handleSubmit = async (e) => {
-        fetch(url, {
-            method: "POST",
-            body: JSON.stringify({
-                name: e.target[0].value,
-                mobileNumber: e.target[1].value,
-                data: e.target[2].value,
-                message: e.target[3].value
-            }),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        })
-    };
-
-    return (
-        <div>
-            <div className='ContactInner'>
-                <div className='ContactShapka'>
-                    <h1 className="headerContactQuestion">
-                        ЗАПИСАТЬСЯ НА ПРИМЕРКУ В ШОУРУМЕ
-                    </h1>
-                    <TbHanger style={iconStyle} />
-                </div>
-                <form onSubmit={handleSubmit} >
-                    <input
-                        style={inputStyle}
-                        value={name}
-                        type="text"
-                        placeholder='Введите имя'
-                        onChange={(e) => setName(e.target.value)}
-                    />
-                    <input
-                        style={{
-                            width: "270px",
-                            height: "60px",
-                            marginLeft: "60px",
-                            padding: "30px"
-                        }}
-                        value={mobileNumber}
-                        type="number"
-                        placeholder='Введите Телефон'
-                        onChange={(e) => setMobileNumber(e.target.value)}
-                    />
-                    <input
-                        className='formInpInfo-aidar'
-                        style={{
-                            height: "70px", padding: "20px",
-                            marginTop: "30px"
-                        }}
-                        value={data}
-                        type="date"
-                        placeholder='Выберите удобное для вас время'
-                        onChange={(e) => setData(e.target.value)}
-                    />
-                    <input
-                        type="text"
-                        style={{
-                            height: "100px", resize: "none", textTransform: "uppercase", padding: "17.5px", display: "block", flexGrow: "1",
-                            width: "350px",
-                            marginTop: "30px"
-                        }}
-                        placeholder='Добавьте комментарий'
-                        onChange={(e) => setMessage(e.target.value)}
-                        value={message}
-                    />
-                    <button style={{
-                        width: "182px",
-                        height: "60px",
-                        transform: "TranslateX(350px) translateY(-100px)"
-                    }}
-                        type='submit'
-                        className='commentConfirmButton'>
-                        ЗАПИСАТЬСЯ
-                    </button>
-                </form>
-            </div>
+  return (
+    <div>
+      <div className="ContactInner">
+        <div className="ContactShapka">
+          <h1 className="headerContactQuestion">
+            ЗАПИСАТЬСЯ НА ПРИМЕРКУ В ШОУРУМЕ
+          </h1>
+          <TbHanger style={iconStyle} />
         </div>
-    )
-}
+        <form onSubmit={handleSubmit}>
+          <div className="formInpInfo">
+            <input
+              style={inputStyle}
+              value={name}
+              type="text"
+              placeholder="Введите имя"
+              onChange={(e) => setName(e.target.value)}
+            />
+            <input
+              value={mobileNumber}
+              type="number"
+              placeholder="Введите Телефон"
+              onChange={(e) => setMobileNumber(e.target.value)}
+            />
+          </div>
 
-export default ContactQuestion
+          <div>
+            <input
+              className="formInpInfo-aidar"
+              value={data}
+              type="date"
+              style={{
+                width: "600px",
+                height: "60px",
+                padding: "20px",
+              }}
+              placeholder="Выберите удобное для вас время"
+              onChange={(e) => setData(e.target.value)}
+            />
+          </div>
+          <div style={{ marginTop: "30px", display: "flex" }}>
+            <textarea
+              type="text"
+              style={{
+                height: "100px",
+                resize: "none",
+                textTransform: "uppercase",
+                padding: "17.5px",
+                display: "block",
+                flexGrow: "1",
+              }}
+              placeholder="Добавьте комментарий"
+              onChange={(e) => setMessage(e.target.value)}
+              value={message}
+            />
+            <button
+              style={{
+                width: "182px",
+                height: "60px",
+              }}
+              type="submit"
+              className="commentConfirmButton"
+            >
+              ЗАПИСАТЬСЯ
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default ContactQuestion;
